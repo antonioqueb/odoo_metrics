5d94c8e5631e9cdbe5f6d457bf8db53f3bddffbf
Antonio Queb <antonioqueb@alphaqueb.online>
2024-12-06 21:11:38 -0600

    Up

--- metrics/collector.py
+++ metrics/collector.py
@@ -6,7 +6,6 @@ def metrics_generator():
   # Build JSON data
   data = {
     "odoo_active_users": len(request.env['res.users'].search([('active', '=', True)])),
-    "odoo_recent_sales": len(request.env['sale.order'].search([('date_order', '>', (fields.Datetime.now() - fields.Datetime.delta(days=1)).strftime('%Y-%m-%d %H:%M:%S'))])),
     "odoo_inventory_items": len(request.env['product.product'].search([]))
   }
 
