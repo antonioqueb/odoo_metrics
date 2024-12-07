4decc4cc7ea3b49c5df12d5ebe5386aff607abfc
Antonio Queb <antonioqueb@alphaqueb.online>
2024-12-06 19:57:30 -0600

    Up

--- controllers/main.py
+++ controllers/main.py
@@ -1,9 +1,11 @@
+# odoo_metrics/controllers/main.py
 from odoo import http
 from odoo.http import request
 from ..metrics.collector import metrics_generator
 
 class MetricsAPI(http.Controller):
 
-    @http.route('/metrics', auth='public', type='http', methods=['GET'])
+    # Consider using Odoo's authentication mechanisms for production
+    @http.route('/metrics', auth='public', type='http', methods=['GET'])  # Adjust auth for production
     def get_metrics(self, **kwargs):
         return metrics_generator()
