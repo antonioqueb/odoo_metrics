84c241661f0b2ec00ee34ee59abfafe891e5315e
Antonio Queb <antonioqueb@alphaqueb.online>
2024-12-07 09:47:16 -0600

    Up

--- controllers/main.py
+++ controllers/main.py
@@ -8,7 +8,19 @@ from ..metrics import product_collector
 
 class MetricsAPI(http.Controller):
 
-    @http.route('/metrics', auth='public', type='http', methods=['GET'])
+    @http.route('/metrics/users', auth='public', type='http', methods=['GET'])
+    def get_users(self, **kwargs):
+        user_data = user_collector.collect_user_data()
+        json_data = json.dumps(user_data, default=str)
+        return Response(json_data, content_type='application/json; charset=utf-8')
+
+    @http.route('/metrics/products', auth='public', type='http', methods=['GET'])
+    def get_products(self, **kwargs):
+        product_data = product_collector.collect_product_data()
+        json_data = json.dumps(product_data, default=str)
+        return Response(json_data, content_type='application/json; charset=utf-8')
+
+    @http.route('/metrics', auth='public', type='http', methods=['GET'])  # Ruta combinada (opcional)
     def get_metrics(self, **kwargs):
         user_data = user_collector.collect_user_data()
         product_data = product_collector.collect_product_data()
